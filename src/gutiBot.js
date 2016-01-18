bc0211568aa9ce77b4156346cf70dc76348e923e
Andrew Seward <awswrd@gmail.com>
2016-01-18 13:15:27 -0800

    Allow Gutibot to supply incoming hook url

--- src/gutiBot.js
+++ src/gutiBot.js
@@ -7,6 +7,14 @@ function _ok(response) {
   return response.status(200);
 }
 
+function _getIncomingWebhookUrl() {
+  return process.env.SLACK_INCOMING_WEBHOOK_URL;
+}
+
+function _getBotUserApiToken() {
+  return process.env.SLACK_BOT_USER_API_TOKEN;
+}
+
 function respondOk(response) {
   return _ok(response).end();
 }
@@ -24,8 +32,15 @@ function respondViaWebhook(hookUrl, channel, message) {
   });
 }
 
+function respondViaDefaultWebhook(channel, message) {
+  const hookUrl = _getIncomingWebhookUrl();
+
+  return respondViaWebhook(hookUrl, channel, message);
+}
+
 module.exports = {
   respondOk,
   respondWith,
   respondViaWebhook,
+  respondViaDefaultWebhook,
 };
