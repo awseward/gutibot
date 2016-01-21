be007e9415e5ac3019fa713f8e6a693eba921d54
Andrew Seward <awswrd@gmail.com>
2016-01-21 06:01:09 -0800

    Add windBot
    It just looks for the word 'wind' anywhere and replies with 'Can you
    paint with all the colors of the wind?'

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const gutibot = require("./gutiBot");
+
 const barelyBot = require("./barelyBot").bot;
 const defineBot = require("./defineBot");
+const windBot = require("./windBot");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,8 +18,9 @@ app.get('/', (req, res) => {
   res.send('Hello! This is gutibot. Are you lost?');
 });
 
-app.post("/doesheknower", gutibot.doSync(barelyBot));
 app.post("/define", defineBot);
+app.post("/doesheknower", gutibot.doSync(barelyBot));
+app.post("/wind", gutibot.doSync(windBot));
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
