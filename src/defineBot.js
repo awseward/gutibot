7189b2337331609512cd5191f18f635f826817f2
Andrew Seward <awswrd@gmail.com>
2016-01-24 16:24:00 -0800

    Add some preliminary refactor notes

--- src/defineBot.js
+++ src/defineBot.js
@@ -1,8 +1,16 @@
 "use strict";
 
+// TODO: Maybe test
+// TODO: Use slack/http utils and whatnot
+// TODO: Potentially pull dictionary lookup stuff out to some util
+
+// TODO: Use import
 const unirest = require('unirest');
+// TODO: Use utils/httpClient
 const request = require('request');
 
+// TODO: Use export
+// TODO: Break this up
 module.exports = function (req, res, next) {
   const botPayload = {};
   let definedWord, definedWords;
@@ -11,14 +19,28 @@ module.exports = function (req, res, next) {
     return res.status(200).send('whops, something went wrong, annoy Michael');
   }
 
+  // TODO: Interpolate
   const requestString = "https://wordsapiv1.p.mashape.com/words/" + wordToDefine + "/definitions";
 
+  // TODO: Pull mashape stuff out
   unirest.get(requestString)
   .header("X-Mashape-Key", "4iIoBDDoMimshMEHtO27Qzs1stjbp1j1yUmjsnVk4z1UHPtrab")
   .header("Accept", "application/json")
   .end(function (result) {
     definedWords = result.body.definitions;
     definedWord = definedWords[Math.floor(Math.random() * definedWords.length)].definition;
+		// TODO: ^^^ Error ^^^
+		// TypeError: Cannot read property 'length' of undefined
+		//     at /app/dist/defineBot.js:19:71
+		//     at Request.handleRequestResponse [as _callback] (/app/node_modules/unirest/index.js:461:25
+		//     at Request.self.callback (/app/node_modules/request/request.js:373:22)
+		//     at emitTwo (events.js:87:13)
+		//     at Request.emit (events.js:172:7)
+		//     at Request.<anonymous> (/app/node_modules/request/request.js:1318:14)
+		//     at emitOne (events.js:82:20)
+		//     at Request.emit (events.js:169:7)
+		//     at IncomingMessage.<anonymous> (/app/node_modules/request/request.js:1266:12)
+		//     at emitNone (events.js:72:20)
 
     botPayload.text = 'The definition of *' + wordToDefine + '* is: \n' +
       '&gt; ' + definedWord;
@@ -27,11 +49,9 @@ module.exports = function (req, res, next) {
     send(botPayload, function (error, status, body) {
       if (error) {
         return next(error);
-
       } else if (status !== 200) {
         // inform user that our Incoming WebHook failed
         return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-
       } else {
         return res.status(200).end();
       }
@@ -39,6 +59,7 @@ module.exports = function (req, res, next) {
   });
 };
 
+// TODO: Use promises
 function send (payload, callback) {
   const uri = 'https://hooks.slack.com/services/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';
 
